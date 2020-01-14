e204a7ef04384b932368c39f77c8fe1bf9340de7
Ben Stretar <ben.stretar@gmail.com>
2020-01-14 13:31:01 -0600

    basic setup to get random gif

---
+++ server/routes/giphy.router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+
+ // get a random gif
+router.get('/', (req, res) => {
+    let apiKey = process.env.GIPHY_API_KEY;
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+        .then(response =>{
+            res.send(response.data.data)
+        })
+        .catch(error =>{
+            console.log(`error getting a random gif`, error)
+            res.sendStatus(500)//internal server error
+        })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
