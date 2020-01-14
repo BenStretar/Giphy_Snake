d7bb2941a977f5331143cf8f33c8ffbc171e6cb8
Ben Stretar <ben.stretar@gmail.com>
2020-01-14 14:49:45 -0600

    adding tables into database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,26 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+"id" SERIAL PRIMARY KEY,
+"username" VARCHAR(80) UNIQUE NOT NULL,
+"password" VARCHAR(1000) NOT NULL,
+"admin" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "gifs" (
+"id" SERIAL PRIMARY KEY,
+"title" VARCHAR(100),
+"url" VARCHAR(300)
+);
+
+CREATE TABLE "user_gifs" (
+"user_id" INT REFERENCES "user",
+"gifs_id" INT REFERENCES "gifs"
+);
+
+CREATE TABLE "game" (
+"id" SERIAL PRIMARY KEY,
+"time_elapsed" TIME,
+"gifs_collected" INT,
+"gifs_id" INT REFERENCES "gifs"
 );
