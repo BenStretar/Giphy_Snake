5b400488629ff8217e6c4d85371b9c15e6187885
Ben Stretar <ben.stretar@gmail.com>
2020-01-15 10:11:25 -0600

    sourced in express and pool

--- server/routes/addedGifs.router.js
+++ server/routes/addedGifs.router.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const pool = require('../modules/pool')
+const express = require('express');
+const pool = require('../modules/pool');
 
 // return added gifs
 router.get('/', (req, res)=>{
