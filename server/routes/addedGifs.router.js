343f34e907f2da202b52a3dbc1209433b8a0d613
Ben Stretar <ben.stretar@gmail.com>
2020-01-14 18:02:13 -0600

    router that returns added gifs

---
+++ server/routes/addedGifs.router.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const pool = require('../modules/pool')
+
+// return added gifs
+router.get('/', (req, res)=>{
+    let queryString = `SELECT * FROM "gifs";`;
+    pool.query(queryString).then(results =>{
+        res.send(results.rows);
+    }).catch(error => {
+        console.log(`Error getting gifs from the database`, error);
+        res.sendStatus(400)
+    });
+});
+
+// // add new gif
+// router.post('/', (req, res) =>{
+
+// })
