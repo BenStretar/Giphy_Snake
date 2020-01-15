caa48df8e29a2dbec86ec6b7499cb710229d9e76
Ben Stretar <ben.stretar@gmail.com>
2020-01-15 10:13:27 -0600

    adminPage working on

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -1,37 +1,45 @@
 import React, { Component } from 'react';
 //import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton'
+import LogOutButton from '../LogOutButton/LogOutButton';
+import './Admin.css'
+
 
 // only an user will admin status will see this page
-export default class AdminPage extends Component {
+class AdminPage extends Component {
+
+    handleDelete = () => {
+        console.log('delete')
+    }
     render() {
         return (
             <div>
                 <h1>Admin Home</h1>
-                {/* 
-                Update when gifs are 'favorited'
+                
                 <section>
                     <table>
                         <thead>
                             <tr>
                                 <th>Title</th>
-                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <tb>gif1</tb>
-                                <tb><button>Delete</button></tb>
-                            </tr>
+                            <tr><tb>phinas and ferb</tb><button>Delete</button></tr>
                         </tbody>
                     </table>
-                </section> */}
+                </section> 
                 <hr/>
                 <section>
-
+                    <input type="text" placeholder="search"/>
+                    <button onClick="searchForGif">Search</button>
+                    <div>
+                        {/* display they gifs that the user searched for limit 12 */}
+                        
+                    </div>
                 </section>
                 <LogOutButton />
             </div>
         )
     }
 }
+
+export default AdminPage;
