7dc4cb5c1b12fb8528977fb0d11ebff807cbced2
Ben Stretar <ben.stretar@gmail.com>
2020-01-16 17:01:16 -0600

    added reducers for favorite and favList

---
+++ src/redux/reducers/favoriteReducer.js
@@ -0,0 +1,12 @@
+const favoriteReducer = (state={
+    title: 'DEFAULT',
+    url: '',
+}, action) => {
+    console.log(action.payload)
+    switch(action.payload){
+        case 'SELECT_IMAGE':
+            return action.payload
+        default:
+            return state
+    }
+}
