bdb9b1ef0c84bdd5597431a294b71f73c73fc2d4
Ben Stretar <ben.stretar@gmail.com>
2020-01-16 17:01:38 -0600

    added sagas for Get, Add, Delete

---
+++ src/redux/sagas/deleteFavorite.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {put, takeLatest} from 'redux-saga/effects';
+
+function* deleteItem(action){
+    try {
+        yield axios.delete('/api/giphy', action.payload);
+        yield put({type: 'GET_FAVORITE_LIST'})
+    } 
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function* deleteFavorite(){
+    yield takeLatest('DELETE_FAVORITE', deleteItem)
+}
+
+export default deleteFavorite;
