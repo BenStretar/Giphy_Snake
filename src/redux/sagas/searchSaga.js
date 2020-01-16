fe50df5a8eb88f1702e504a67a374ddab79b6c52
Ben Stretar <ben.stretar@gmail.com>
2020-01-15 22:36:13 -0600

    updated search saga

--- src/redux/sagas/searchSaga.js
+++ src/redux/sagas/searchSaga.js
@@ -4,15 +4,15 @@ import {put, takeLatest} from 'redux-saga/effects';
 function* search(){
     try {
         const getResponse = yield axios.get(`/api/giphy/search/${action.payload}`);
-        yield put({ type: 'SET_SEARCH', payload: getResponse.data})
+        yield put({ type: 'GET_SEARCH', payload: getResponse.data})
     }
     catch (error) {
-        console.log(error)
+        console.log('error getting search:', error)
     }
 }
 
 function* searchSaga(){
-    yield takeLatest('GET_SEARCH', search)
+    yield takeLatest('SEARCH_GIPHY', search)
 }
 
 export default searchSaga;
