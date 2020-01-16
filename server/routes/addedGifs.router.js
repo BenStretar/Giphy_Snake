e37b63761e609da91764102cb05b559891f4fbda
Ben Stretar <ben.stretar@gmail.com>
2020-01-16 14:21:21 -0600

    updated addedGifs Router

--- server/routes/addedGifs.router.js
+++ server/routes/addedGifs.router.js
@@ -12,7 +12,34 @@ router.get('/', (req, res)=>{
     });
 });
 
-// // add new gif
-// router.post('/', (req, res) =>{
+// add a new favorite 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const {image_url, title} = req.body;
+    let queryString = `INSERT INTO "gifs" ("title", "url") VALUES ($1, $2);`;
+  
+    pool.query(queryString, [title,image_url]).then(result=>{
+      console.log('ADD RESULT',result);
+      
+      res.sendStatus(200);
+    }).catch(error=>{
+      res.sendStatus(400);
+      console.log(error);    
+    })
+  });
 
-// })
+  // delete a favorite
+router.delete('/:id', (req, res) => {
+    
+    let queryString = `DELETE FROM "gif" WHERE "id"=$1;`;
+  
+    //Query to dependant tables
+    pool.query(queryString, [req.params.id]).then(result=>{
+      
+    }).catch(error=>{
+      console.log('Error deleting favorites from database:',error);
+      res.sendStatus(400);
+    });
+  });
+  
+  module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-const giphyRouter = require(`./routes/giphy.router`); //gifs - right now very basic set up to get one at random
-
+const giphyRouter = require(`./routes/giphy.router`); 
+const addGifsRouter = require('./routes/addedGifs.router');
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use(`/api/giphy`, giphyRouter); //gifs
+app.use(`/api/addGif`, addGifsRouter);
 
 // Serve static files
 app.use(express.static('build'));
