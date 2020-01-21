83e5cf247ea914f7c2c83be7b5f3b07cf6528d0b
Ben Stretar <ben.stretar@gmail.com>
2020-01-21 11:58:33 -0600

    hid some rules that aren't in game yet

--- src/components/HowToPlay/HowToPlay.js
+++ src/components/HowToPlay/HowToPlay.js
@@ -4,7 +4,7 @@ class HowToPlay extends Component {
     render() {
         return (
             <>
-                <h1>This is the How to Play page</h1>
+                <h1>How to Play</h1>
                 <div>
                     {/* just a screen shot of the game board?
                     or draw it out?
@@ -12,12 +12,13 @@ class HowToPlay extends Component {
 
                     <div className="">
                         <ul>
-                            <li>Use the Arrow Keys on your keyboard to move the blue square around the board.</li>
-                            <li>Avoid the red rectangles. If they hit you your game will end.</li>
+                            <li>Use the Arrow Keys on your keyboard to move the black square around the board</li>
+                            <li>If you run into the edges of the board your game will end</li>
+                            {/* <li>Avoid the red rectangles. If they hit you your game will end.</li> */}
                             <li>Collect as many gifs (items) as you can</li>
                             <li>Top left of the screen shows your time and below that the amount of gifs that you have picked up</li>
-                            <li>note: you don't have to pick up this items if you don't want to</li>
-                            <li>the longer you survive the faster the enemies will move</li>
+                            {/* <li>note: you don't have to pick up this items if you don't want to</li> */}
+                            {/* <li>the longer you survive the faster the enemies will move</li> */}
                         </ul>
                     </div>
                 </div>
