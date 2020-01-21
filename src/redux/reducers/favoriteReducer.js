14c84f619bb3fb4c8ef9b5c6ffeb655f8b34df30
Ben Stretar <ben.stretar@gmail.com>
2020-01-21 15:46:43 -0600

    rewrote favReducer

--- src/redux/reducers/favoriteReducer.js
+++ src/redux/reducers/favoriteReducer.js
@@ -1,11 +1,13 @@
-const favoriteReducer = (state={title: 'DEFAULT', image_url: '', categories: []}, action) => {
+const favoriteReducer = (state={
+    title: 'DEFAULT',
+    image_url: '',
+    categories: []
+  }, action) => {
     console.log(action.payload)
-    switch(action.payload){
-        case 'SELECT_IMAGE':
-            return action.payload
-        default:
-            return state
+    switch (action.type) {
+      case 'SELECT_IMAGE': return action.payload
+      default: return state;
     }
-}
+  }
 
 export default favoriteReducer;
