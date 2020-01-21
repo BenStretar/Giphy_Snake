cdf65e6eb6f35455e60d354a01fc062ad494049b
Ben Stretar <ben.stretar@gmail.com>
2020-01-21 15:48:57 -0600

    addd console logs

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -5,24 +5,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 const axios = require('axios');
 
-/**
+/*
  * GET route template
  */
 
  // search for a gif
 router.get('/search/:searchQuery', (req, res) => {
     let apiKey = process.env.GIPHY_API_KEY;
-    console.log(req.params.searchQuery)
+    console.log('you searched for: ', req.params.searchQuery)
     axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${req.params.searchQuery}&limit=12`)
         .then(response =>{
             searchResults = [];
             for (let item of response.data.data){
                 searchResults.push({image_url: item.images.original.url, title: item.title})
             }
+            
+            console.log(searchResults)
             res.send(searchResults)
         })
         .catch(error =>{
-            console.log(`error getting a random gif`, error)
+            console.log(`error searching for a gif`, error)
             res.sendStatus(500)//internal server error
         });
 });
