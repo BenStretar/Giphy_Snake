3d7f89b9e265c91c899bc671d7b663cc04c029f9
Ben Stretar <ben.stretar@gmail.com>
2020-01-25 16:46:44 -0600

    added styling to pages

--- src/components/AddGifPage/addGifPage.js
+++ src/components/AddGifPage/addGifPage.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import GifCard from '../GifCard/GifCard';
+import {withStyles} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import 'typeface-roboto';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+      },
+      dense: {
+        marginTop: 16,
+      },
+      menu: {
+        width: 200,
+      },
+      button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none'
+    },
+  });
 
 class addGifPage extends Component {
-
+    
     state = {
         search: ''
     }
@@ -18,16 +45,27 @@ class addGifPage extends Component {
     searchForGif = ()=>{
         this.props.dispatch({type: 'SEARCH_GIPHY', payload: this.state.search})
     }
+
+    goHome = ()=>{
+        this.props.history.push('/admin')
+    }
    
     render() {
         return (
             <div>
-                <h1>Search Page</h1>
-                <input placeholder="search" type="text" onChange={(event)=>this.handleChange(event)} 
-                value={this.state.search}/>
+                {/* <h1>Search for an Image</h1> */}
+                <TextField 
+                    id="outlined-name"
+                    label="search" 
+                    type="text" onChange={(event)=>this.handleChange(event)} 
+                    value={this.state.search}
+                    margin="normal"
+                    variant="outlined"
+                /> <br/>
 
-                <button onClick={this.searchForGif}>Search</button>
-                                
+                <Button variant="outlined" onClick={this.goHome}>Cancel</Button>
+                <Button color="secondary" variant="contained" onClick={this.searchForGif}>Search</Button>
+                                                
                     {/* display 12 gifs that the user searched for */}
                     {/* {JSON.stringify(this.props.images)} */}
                     <div className="pictureBox">
@@ -40,4 +78,4 @@ class addGifPage extends Component {
     }
 }
 
-export default connect(reduxState=>({images: reduxState.searchReducer}))(addGifPage);
+export default connect(reduxState=>({images: reduxState.searchReducer}))(withStyles(styles)(addGifPage));
